c8a9e2047cef14f0509d3dee705a9511b600c0cb
ifpr <guidesjose39@gmail.com>
2023-04-26 17:19:02 -0300

    Conversão R-D 26/04

--- app/src/main/java/ifpr/btn/Real_dolar.java
+++ app/src/main/java/ifpr/btn/Real_dolar.java
@@ -3,12 +3,43 @@ package ifpr.btn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Real_dolar extends AppCompatActivity {
+
+    Button btnConv;
+    EditText etReal;
+    TextView txtReal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_dolar);
+
+
+        btnConv = findViewById(R.id.btnConv);
+        etReal = findViewById(R.id.etReal);
+        txtReal= findViewById(R.id.txtReal);
+
+        btnConv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String real = etReal.getText().toString();
+                double d1 = Double.parseDouble(real);
+                double r = d1 * 5.0436d;
+                String result = String.valueOf(r);
+
+
+
+                txtReal.setText(result);
+
+
+            }
+        });
+
     }
 }
--- app/src/main/java/ifpr/btn/TelaB3.java
+++ app/src/main/java/ifpr/btn/TelaB3.java
@@ -6,17 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class TelaB3 extends AppCompatActivity {
 
     Button btnV3;
+    Button btnReal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_b3);
 
         btnV3=findViewById(R.id.btnV3);
+        btnReal=findViewById(R.id.btnReal);
+
+        btnReal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(TelaB3.this, Real_dolar.class);
+                startActivity(it);
+            }
+        });
         btnV3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
