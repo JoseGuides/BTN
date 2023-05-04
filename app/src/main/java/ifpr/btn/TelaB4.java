495f1452c9bcc48d2f28c20e85b3f78768a476f4
ifpr <guidesjose39@gmail.com>
2023-05-04 13:01:52 -0300

    IMC completo

--- app/src/main/java/ifpr/btn/TelaB4.java
+++ app/src/main/java/ifpr/btn/TelaB4.java
@@ -6,10 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class TelaB4 extends AppCompatActivity {
 
     Button btnV4;
+    Button btnCalc;
+    EditText etAltura;
+    EditText etPeso;
+    TextView txtResult;
 
 
     @Override
@@ -18,6 +26,31 @@ public class TelaB4 extends AppCompatActivity {
         setContentView(R.layout.activity_tela_b4);
 
         btnV4=findViewById(R.id.btnV4);
+        btnCalc=findViewById(R.id.btnCalc);
+        etAltura=findViewById(R.id.etAltura);
+        etPeso=findViewById(R.id.etPeso);
+        txtResult=findViewById(R.id.txtResult);
+
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String alt = etAltura.getText().toString();
+                double d1 = Double.parseDouble(alt);
+                String peso = etPeso.getText().toString();
+                double d2 = Double.parseDouble(peso);
+                double r = d2 / (d1 * d1);
+
+
+
+                DecimalFormat df = new DecimalFormat("0.00");
+
+
+
+
+               txtResult.setText(df.format(r));
+            }
+        });
         btnV4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
