94164c9119ea34726306b59e098b5bbac44f2312
ifpr <guidesjose39@gmail.com>
2023-05-11 12:52:08 -0300

    Gerador quase pronto

--- app/src/main/java/ifpr/btn/TelaB6.java
+++ app/src/main/java/ifpr/btn/TelaB6.java
@@ -6,9 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 public class TelaB6 extends AppCompatActivity {
     Button btnV6;
+
+    Button btnGerar;
+
+    TextView txtNumeros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,20 @@ public class TelaB6 extends AppCompatActivity {
         setContentView(R.layout.activity_tela_b6);
 
         btnV6=findViewById(R.id.btnV6);
+        btnGerar=findViewById(R.id.btnGerar);
+        txtNumeros=findViewById(R.id.txtNumeros);
+
+        btnGerar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                String num = String.valueOf(Math.random());
+
+                txtNumeros.setText(num);
+            }
+        });
         btnV6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
